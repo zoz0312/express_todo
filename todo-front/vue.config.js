46964a92456bfdde7788ae3869ac1a810cf0ec30
zoz0312@naver.com <zoz0312@naver.com>
2020-06-02 23:58:35 +0900

    Front Vue add

---
+++ todo-front/vue.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+	],
+	/*
+	'css': {
+		'loaderOptions': {
+			'sass': {
+				'data': `
+					@import "@/assets/scss/main.scss";
+				`
+			}
+		}
+	},
+	*/
+	'outputDir': path.resolve(__dirname, '../myapp-node/public'),
+	'devServer': {
+		'proxy': {
+			'/api': {
+				'target': 'http://localhost:4000/api',
+				'changeOrigin': true,
+				'pathRewrite': {
+					'^/api': ''
+				}
+			}
+		}
+	}
+}
