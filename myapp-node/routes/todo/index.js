612f875ab6fbb496490db2e8c7e7a122586458b7
zoz0312@naver.com <zoz0312@naver.com>
2020-06-07 23:48:29 +0900

    Vue BootStrap Add

--- myapp-node/routes/todo/index.js
+++ myapp-node/routes/todo/index.js
@@ -6,6 +6,8 @@ const lib = new libs();
 
 const { TodoCard } = require('../models');
 
+const { todoCVali, todoC } = require('./create');
+
 router.post('/selectAll', async (req, res, next) => {
 	try {
 		lib.success = true;
@@ -16,56 +18,7 @@ router.post('/selectAll', async (req, res, next) => {
 	res.send(lib.resData());
 });
 
-router.post('/create', (req, res, next) => {
-	const title = req.body('title');
-	const contents = req.body('contents');
-	const dueDate = req.body('dueDate');
-	const type = req.body('type');
-	const depth = req.body('depth');
-
-	let flag = false;
-
-	if (!!title == false || typeof title !== 'string') {
-		flag = true;
-	}
-	if (!!contents == false || typeof contents !== 'string') {
-		flag = true;
-	}
-	if (!!dueDate == false || typeof dueDate !== 'string') {
-		flag = true;
-	}
-	if (!!type == false || typeof type !== 'string') {
-		flag = true;
-	}
-	if (!!depth == false || typeof depth !== 'number') {
-		flag = true;
-	}
-
-	if (flag) {
-		lib.errDesc = 'Type Error';
-		res.send(lib.resData());
-	} else {
-		next();
-	}
-});
-
-router.post('/create', async (req, res, next) => {
-	const data = {
-		title: req.body('title'),
-		contents: req.body('contents'),
-		dueDate: req.body('dueDate'),
-		type: req.body('type'),
-		depth: req.body('depth'),
-		createDate: new Date(),
-		updateDate: new Date(),
-	}
-	try {
-		lib.success = true;
-		lib.data = await TodoCard.create(data);
-	} catch (e) {
-		lib.errDesc = e;
-	}
-	res.send(lib.resData());
-});
+router.post('/create', todoCVali);
+router.post('/create', todoC);
 
 module.exports = router;
